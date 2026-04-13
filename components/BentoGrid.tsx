d520bb7ba615a7f6fe032ca06a86ea39c15268ef
isamasih007 <abhayrawat1094@gmail.com>
2026-04-14 01:14:05 +0530

    Create BentoGrid.tsx

---
+++ components/BentoGrid.tsx
@@ -0,0 +1,7 @@
+export default function BentoGrid({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {children}
+    </div>
+  );
+}
